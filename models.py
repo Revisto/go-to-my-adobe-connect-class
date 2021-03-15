86061302fca7cf86d4e9ceb3704151e01d1e7f37
Revisto <walt.shabani@gmail.com>
2021-03-15 09:52:01 +0330

    rename function setup_selenium_drive_with_android_useragent, add feature to log-in with fake useragent android

--- models.py
+++ models.py
@@ -3,8 +3,7 @@ from os import getenv
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
- 
-
+from selenium.webdriver.chrome.options import Options
 class SeleniumAutomation:
     def __init__(self):
         load_dotenv()
@@ -13,8 +12,10 @@ class SeleniumAutomation:
         self.adobe_connect_password = getenv("adobe_connect_password")
         self.chrome_webdriver_path = getenv("chrome_webdriver_path")
     
-    def setup_selenium_drive(self):
-        self.driver = webdriver.Chrome(self.chrome_webdriver_path)
+    def setup_selenium_drive_with_android_useragent(self):
+        options = Options()
+        options.add_argument("user-agent=Mozilla/5.0 (Linux; Android 6.0; HTC One M9 Build/MRA58K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.98 Mobile Safari/537.36")
+        self.driver = webdriver.Chrome(executable_path=self.chrome_webdriver_path, chrome_options=options)
 
     def go_to_my_adobe_connect_class_url(self):
         self.driver.get(self.adobe_connect_class_url)
