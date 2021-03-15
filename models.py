564bbd70f846e881cc1063efea9780ca552ed916
Revisto <walt.shabani@gmail.com>
2021-03-15 09:22:36 +0330

    add chrome_webdriver_path to SeleniumAutomation class init

--- models.py
+++ models.py
@@ -8,4 +8,5 @@ class SeleniumAutomation:
         self.adobe_connect_class_url = getenv("adobe_connect_class_url")
         self.adobe_connect_username = getenv("adobe_connect_username")
         self.adobe_connect_password = getenv("adobe_connect_password")
-        
+        self.chrome_webdriver_path = getenv("chrome_webdriver_path")
+    
