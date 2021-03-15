5fda88c7d6e4500f8f9bfb098c0890fce1736380
Revisto <walt.shabani@gmail.com>
2021-03-15 12:47:02 +0330

    add delay function, add log_in_into_adobe_connect_lass function

--- models.py
+++ models.py
@@ -4,6 +4,7 @@ from os import getenv
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
+from time import sleep
 class SeleniumAutomation:
     def __init__(self):
         load_dotenv()
@@ -11,7 +12,11 @@ class SeleniumAutomation:
         self.adobe_connect_username = getenv("adobe_connect_username")
         self.adobe_connect_password = getenv("adobe_connect_password")
         self.chrome_webdriver_path = getenv("chrome_webdriver_path")
+        self.delay_time = 3
     
+    def delay(self):
+        sleep(self.delay_time)
+
     def setup_selenium_drive_with_android_useragent(self):
         options = Options()
         options.add_argument("user-agent=Mozilla/5.0 (Linux; Android 6.0; HTC One M9 Build/MRA58K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.98 Mobile Safari/537.36")
@@ -19,3 +24,11 @@ class SeleniumAutomation:
 
     def go_to_my_adobe_connect_class_url(self):
         self.driver.get(self.adobe_connect_class_url)
+        SeleniumAutomation().delay()
+
+    def log_in_into_adobe_connect_class(self):
+        self.driver.find_element_by_name("login").send_keys(self.adobe_connect_username)
+        self.driver.find_element_by_name("password").send_keys(self.adobe_connect_password)
+        self.driver.find_element_by_xpath("//input[@type='submit']").click()
+        SeleniumAutomation().delay()
+        self.driver.find_elements_by_class_name("open-in-browser-button")[-1].click()
