f34afbe6cf0e2c3be9c7c02906e3852cae1cbbd2
Revisto <walt.shabani@gmail.com>
2021-03-16 12:58:00 +0330

    make type_something_in_chatbar function and make the chrome headless

--- models.py
+++ models.py
@@ -4,6 +4,10 @@ from os import getenv
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 from validator_collection import *
 from time import sleep
 class SeleniumAutomation:
@@ -18,11 +22,11 @@ class SeleniumAutomation:
     def delay(self, delay_time = None):
         if is_none(delay_time):
             delay_time = self.delay_time
-        
         sleep(self.delay_time)
 
     def setup_selenium_drive_with_android_useragent(self):
         options = Options()
+        options.add_argument("--headless")  
         options.add_argument("user-agent=Mozilla/5.0 (Linux; Android 6.0; HTC One M9 Build/MRA58K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.98 Mobile Safari/537.36")
         self.driver = webdriver.Chrome(executable_path=self.chrome_webdriver_path, chrome_options=options)
 
@@ -36,4 +40,9 @@ class SeleniumAutomation:
         self.driver.find_element_by_xpath("//input[@type='submit']").click()
         SeleniumAutomation().delay()
         self.driver.find_elements_by_class_name("open-in-browser-button")[-1].click()
-        #SeleniumAutomation().delay(30)
+        
+    def type_something_in_chatbar(self, text):
+        self.driver.find_elements_by_class_name("spectrum-Button")[1].click() 
+        textarea = self.driver.find_element_by_css_selector("textarea")
+        textarea.send_keys(text)
+        textarea.send_keys(Keys.RETURN)
